5436ab64b5d584eff42430c46e6564e1b8687ed6
rajvi0106 <rajvisinghrathore001@gmail.com>
2026-07-16 01:37:02 +0530

    fix auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app=express();
@@ -11,6 +12,7 @@ app.use(cors());
 
 connectDB();
 
+app.use("/api/auth",authRoutes)
 app.get("/",(req,res)=>{
     res.json({ message: 'Server is running' });
 })
